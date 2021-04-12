ea0bb8d658162960b94899d166998eaa27a371fd
brian <briansegs@gmail.com>
2021-04-12 09:31:00 -0400

    Init: Add lesson files

---
+++ jsx/files/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myList = (
+  <ul>
+    <li>red</li>
+    <li>blue</li>
+    <li>yellow</li>
+  </ul>
+);
+ReactDOM.render(
+  myList,
+  document.getElementById('app')
+);
